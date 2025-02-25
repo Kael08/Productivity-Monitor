76b07dbc5a4eb5666a4d78a0588c9fd6eb85b359
Kael08 <mingiyan.1999@mail.ru>
2025-02-25 15:30:03 +0300

    добавление и удаление процессов, запуск монитора для блокировки процессов

--- src/main/java/productivityMonitor/controllers/MainController.java
+++ src/main/java/productivityMonitor/controllers/MainController.java
@@ -6,13 +6,16 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.Timer;
-import java.util.TimerTask;
+import java.util.*;
+import java.util.regex.*;
 
 public class MainController {
     @FXML
@@ -47,10 +50,33 @@ public class MainController {
     private Label clockLabel; // Часы
 
     @FXML
+    private TextField inputTextField; // Поле ввода в консоли
+
+    List<String> processList = new ArrayList<>(); // Хранит имена блокируемых процессов
+
+    // Флаг для работы монитора
+    boolean runFlag = false;
+
+    // Поток для работы монитора
+    private Thread runThread;
+
+    @FXML
     private void handleRunButton(ActionEvent event) {
         System.out.println("Кнопка Run нажата!");
-        consoleTextArea.appendText("RUN"+"\n");
 
+        if (!runFlag) {
+            consoleTextArea.appendText("Монитор запущен!\n");
+            runFlag = true;
+            runThread = new Thread(runMonitor);
+            runThread.start();
+        } else {
+            consoleTextArea.appendText("Монитор остановлен!\n");
+            runFlag = false;
+            if (runThread != null) {
+                runThread.interrupt(); // Прерываем поток корректно
+                runThread = null;
+            }
+        }
     }
 
     @FXML
@@ -68,13 +94,13 @@ public class MainController {
     @FXML
     private void handleDeleteButton(ActionEvent event) {
         System.out.println("Кнопка Delete нажата!");
-        consoleTextArea.appendText("DELETE"+"\n");
+        deleteProcess(inputTextField.getText());
     }
 
     @FXML
     private void handleAddButton(ActionEvent event) {
         System.out.println("Кнопка Add нажата!");
-        consoleTextArea.appendText("ADD"+"\n");
+        addProcess(inputTextField.getText());
     }
 
     @FXML
@@ -109,6 +135,85 @@ public class MainController {
 
         label.setText(currentTime);
     }
+
+    // Закрывает процессы по имени
+    private void closeProcess(List<String> list) {
+        for (String pn : list) {
+            try {
+                ProcessBuilder builder = new ProcessBuilder("taskkill", "/IM", pn, "/F");
+                Process process = builder.start();
+                int exitCode = process.waitFor();
+
+                if (exitCode == 0) {
+                    consoleTextArea.appendText("Процесс " + pn + " был завершен\n");
+                } else {
+                    // Это также вызывается, когда процесса не было или он не найден, и из-за этого мусорится консоль
+                    //consoleTextArea.appendText("Ошибка при завершении процесса " + pn + "\n");
+                }
+            } catch (Exception e) {
+                consoleTextArea.appendText("Не удалось завершить процесс " + pn + ": " + e.getMessage() + "\n");
+            }
+        }
+    }
+
+
+    // Задача для закрытия процессов
+    Runnable runMonitor = () -> {
+        while(runFlag){
+            try {
+                closeProcess(processList);
+                Thread.currentThread().sleep(5000);
+                System.out.println("Ещё один цикл");
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt(); // Устанавливаем флаг прерывания
+                System.out.println("Мониторинг был прерван.");
+                return; // Прерываем выполнение потока
+            }
+        }
+    };
+    // Добавление процесса в список блокируемых процессов
+    private void addProcess(String processName){
+        if(processName.isEmpty()) {
+            consoleTextArea.appendText("Ошибка: Название процесса не может быть пустым!\n");
+            return;
+        } else if(processName.equals(".exe")) {
+            consoleTextArea.appendText("Ошибка: Введите название процесса!\n");
+            return;
+        }
+
+        if(!processName.endsWith(".exe")){
+            processName+=".exe";
+        }
+
+        consoleTextArea.appendText(processName+"\n");
+        processList.add(processName);
+    }
+
+    // Удаление процесса из списка блокируемых процессов
+    private void deleteProcess(String processName){
+        if(processName.isEmpty()) {
+            consoleTextArea.appendText("Ошибка: Название процесса не может быть пустым!\n");
+            return;
+        }
+
+        if(!processName.endsWith(".exe")){
+            processName+=".exe";
+        } else if(processName.equals(".exe")) {
+            consoleTextArea.appendText("Ошибка: Введите название процесса!\n");
+            return;
+        }
+
+        final String copyProcessName = processName;
+
+        boolean isRemoved =  processList.removeIf(process->process.equals(copyProcessName));
+
+        if(isRemoved){
+            consoleTextArea.appendText("Процесс успешно удален!\n");
+        } else {
+            consoleTextArea.appendText("Ошибка: Процесс с именем '" + processName + "' не найден!\n");
+        }
+    }
+
 
 
 }
