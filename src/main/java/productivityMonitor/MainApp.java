9f7efbedd9a76e017fc028245ab46fb01844d888
Kael08 <mingiyan.1999@mail.ru>
2025-03-01 13:03:28 +0300

    view для консоли настройки процессов

--- src/main/java/productivityMonitor/MainApp.java
+++ src/main/java/productivityMonitor/MainApp.java
@@ -12,10 +12,11 @@ public class MainApp extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(MainApp.class.getResource("/fxml/mainView.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 1920, 1080);
+        Scene scene = new Scene(fxmlLoader.load());
 
         stage.getIcons().add(new Image(MainApp.class.getResourceAsStream("/images/icon.png")));
         stage.setTitle("Productivity Monitor");
+
 
         stage.setScene(scene);
         stage.show();
